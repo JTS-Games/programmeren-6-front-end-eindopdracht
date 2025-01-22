099bdc06dd3d296ff2da8fb204560111337fb04c
JTS-Games <1066300@hr.nl>
2025-01-22 12:45:20 +0100

    Made main and create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-function App() {
+import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Layout from './Layout.jsx'
+import Home from './home.jsx'
+import Spots from './spots.jsx'
+import SpotCreate from './spotcreate.jsx'
+import SpotDetail from './spotdetail.jsx'
+import Delete from "./Delete.jsx";
+import SpotEdit from "./Edit.jsx";
 
-  return (
-    <>
-        <p>Test</p>
-    </>
-  )
+const router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home/>,
+            },
+            {
+                path: '/spots',
+                element: <Spots/>,
+            },
+            {
+                path: '/spots/create',
+                element: <SpotCreate/>,
+            },
+            {
+                path: '/spots/:id',
+                element: <SpotDetail/>,
+            },
+            {
+                path: '/delete/:id',
+                element: <Delete/>,
+            },
+            {
+                path: '/edit/:id',
+                element: <SpotEdit/>,
+            },
+        ]
+    }
+])
+function App() {
+  return <RouterProvider router={router}/>
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['"Comic Sans MS"', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
